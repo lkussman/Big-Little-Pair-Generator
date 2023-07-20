4e24152063b0fa34a7a3e2d74e04ced3bf1a0ae2
lkussman <122315640+lkussman@users.noreply.github.com>
2023-07-20 17:22:51 -0500

    Add files via upload

--- Pairs.cpp
+++ Pairs.cpp
@@ -60,56 +60,71 @@ void Pairs::read_in() {
     
     // pair up all the people who put each other first
     first_pairs();
+    //pair up people who wanted each other as second or third choices
     second_pairs();
     third_pairs();
-   
-    
-    cout << "hi"; 
+    //pair up people who have each other as their second choice
+    fourth_pairs();
+   //pair up when the big as 2 and the little as 3
+    fifth_pairs();
+    //pair up when the little has 2 and the big has 3
+    sixth_pairs();
+    //pair up when they are 3 and 3
+    seventh_pairs();
+    //anyone who is on each others list
+    eighth_pairs();
+    //pair the leftovers
+    random_pairs();
+    write_out();
 }
 
 void Pairs::first_pairs() {
     
     auto iter = Littles.begin();
     
+    //loop through the littles
     while (iter != Littles.end()) {
-        
+        //find the littles first pick and who the corresponding Big put in as their first pick
         string first_pick = iter->second[0];
-        string other_pick = Bigs[first_pick][0];
-    
-        if (iter->first == other_pick) {
-            
-            pair<string, string> pairing;
-            
-            pairing.first = first_pick;
-            pairing.second = Bigs[first_pick][0];
-            
-            duos.push_back(pairing);
-            
-            auto temp = iter->first;
+        
+        //check if the first pic is deleted
+        if (Bigs.find(first_pick) == Bigs.end()) {
             iter++;
-            
-            Littles.erase(temp);
-            Bigs.erase(first_pick);
-            
         }
+        
         else {
-            iter++;
+            string other_pick = Bigs[first_pick][0];
+            
+            //if they chose each other, make them a pair and remove them both from their maps
+            if (iter->first == other_pick) {
+                //big and little pairing
+                vector<string> pairing;
+                pairing.resize(2);
+                
+                pairing[0] = first_pick;
+                pairing[1] = Bigs[first_pick][0];
+                
+                duos.push_back(pairing);
+                
+                auto temp = iter->first;
+                iter++;
+                
+                Littles.erase(temp);
+                Bigs.erase(first_pick);
+                
+            }
+            else {
+                iter++;
+            }
         }
-        
-        
-        
     }
-    
 }
 
 void Pairs::second_pairs() {
     
-    // loop through the Bigs and if their first choice has put them either second or third then pair
-    
     auto iter = Bigs.begin();
-    
+    //loop through all the bigs
     while (iter != Bigs.end()) {
-        
         
         string first_pick = iter->second[0];
         
@@ -125,10 +140,11 @@ void Pairs::second_pairs() {
         
             if (iter->first == other_pick) {
                 
-                pair<string, string> pairing;
+                vector<string> pairing;
+                pairing.resize(2);
                 
-                pairing.first = first_pick;
-                pairing.second = other_pick;
+                pairing[0] = other_pick;
+                pairing[1] = first_pick;
                 
                 duos.push_back(pairing);
                 
@@ -142,10 +158,11 @@ void Pairs::second_pairs() {
             else if (iter->first == other_pick2) {
                 
                 
-                pair<string, string> pairing;
+                vector <string> pairing;
+                pairing.resize(2);
                 
-                pairing.first = first_pick;
-                pairing.second = other_pick2;
+                pairing[0] = first_pick;
+                pairing[1] = other_pick2;
                 
                 duos.push_back(pairing);
                 
@@ -159,12 +176,8 @@ void Pairs::second_pairs() {
             else {
                 iter++;
             }
-            
         }
-       
-        
     }
-    
 }
 
 void Pairs::third_pairs() {
@@ -188,10 +201,11 @@ void Pairs::third_pairs() {
         
             if (iter->first == other_pick) {
                 
-                pair<string, string> pairing;
+                vector<string> pairing;
+                pairing.resize(2);
                 
-                pairing.first = first_pick;
-                pairing.second = other_pick;
+                pairing[0] = first_pick;
+                pairing[1] = other_pick;
                 
                 duos.push_back(pairing);
                 
@@ -205,10 +219,11 @@ void Pairs::third_pairs() {
             else if (iter->first == other_pick2) {
                 
                 
-                pair<string, string> pairing;
+                vector<string> pairing;
+                pairing.resize(2);
                 
-                pairing.first = first_pick;
-                pairing.second = other_pick2;
+                pairing[0] = first_pick;
+                pairing[1] = other_pick2;
                 
                 duos.push_back(pairing);
                 
@@ -222,16 +237,252 @@ void Pairs::third_pairs() {
             else {
                 iter++;
             }
-            
-            
-    
-            
         }
-        
-        
-        
     }
-    
 }
 
+void Pairs::fourth_pairs() {
+    auto iter = Littles.begin();
+    
+    //loop through the littles
+    while (iter != Littles.end()) {
+        //find the littles second pick and who the corresponding Big put in as their second pick
+        string second_pick = iter->second[1];
+        
+        //check if the first pic is deleted
+        if (Bigs.find(second_pick) == Bigs.end()) {
+            iter++;
+        }
+        
+        else {
+            string other_pick = Bigs[second_pick][1];
+            
+            //if they chose each other, make them a pair and remove them both from their maps
+            if (iter->first == other_pick) {
+                //big and little pairing
+                vector<string> pairing;
+                pairing.resize(2);
+                
+                pairing[0] = second_pick;
+                pairing[1] = Bigs[second_pick][1];
+                
+                duos.push_back(pairing);
+                
+                auto temp = iter->first;
+                iter++;
+                
+                Littles.erase(temp);
+                Bigs.erase(second_pick);
+                
+            }
+            else {
+                iter++;
+            }
+        }
+    }
+}
 
+void Pairs::fifth_pairs() {
+    auto iter = Bigs.begin();
+    //loop through all the bigs
+    //bigs second choice littles third
+    while (iter != Bigs.end()) {
+        
+        string first_pick = iter->second[1];
+        
+        if (Littles.find(first_pick) == Littles.end()) {
+            ++iter;
+           
+        }
+        else {
+            
+            string other_pick = Littles[first_pick][2];
+        
+            if (iter->first == other_pick) {
+                
+                vector<string> pairing;
+                pairing.resize(2);
+                
+                pairing[0] = other_pick;
+                pairing[1] = first_pick;
+                
+                duos.push_back(pairing);
+                
+                auto temp = iter->first;
+                iter++;
+                
+                Bigs.erase(temp);
+                Littles.erase(first_pick);
+                
+            }
+            else {
+                iter++;
+            }
+        }
+    }
+}
+
+void Pairs::sixth_pairs() {
+    auto iter = Littles.begin();
+    //loop through all the bigs
+    while (iter != Littles.end()) {
+        
+        string first_pick = iter->second[1];
+        
+        if (Bigs.find(first_pick) == Bigs.end()) {
+            ++iter;
+           
+        }
+        else {
+            
+            string other_pick = Bigs[first_pick][2];
+        
+            if (iter->first == other_pick) {
+                
+                 vector<string> pairing;
+                pairing.resize(2);
+                
+                pairing[0] = first_pick;
+                pairing[1] = other_pick;
+                
+                duos.push_back(pairing);
+                
+                auto temp = iter->first;
+                iter++;
+                
+                Littles.erase(temp);
+                Bigs.erase(first_pick);
+                
+            }
+            else {
+                iter++;
+            }
+        }
+    }
+}
+
+void Pairs::seventh_pairs() {
+    //bigs third and littles third
+    auto iter = Littles.begin();
+    
+    //loop through the littles
+    while (iter != Littles.end()) {
+        //find the littles second pick and who the corresponding Big put in as their second pick
+        string second_pick = iter->second[2];
+        
+        //check if the first pic is deleted
+        if (Bigs.find(second_pick) == Bigs.end()) {
+            iter++;
+        }
+        
+        else {
+            string other_pick = Bigs[second_pick][2];
+            
+            //if they chose each other, make them a pair and remove them both from their maps
+            if (iter->first == other_pick) {
+                //big and little pairing
+               vector<string> pairing;
+                pairing.resize(2);
+                
+                pairing[0] = second_pick;
+                pairing[1] = Bigs[second_pick][2];
+                
+                duos.push_back(pairing);
+                
+                auto temp = iter->first;
+                iter++;
+                
+                Littles.erase(temp);
+                Bigs.erase(second_pick);
+                
+            }
+            else {
+                iter++;
+            }
+        }
+    }
+}
+void Pairs::eighth_pairs() {
+    //if anyone is on each others lists, pair them
+    auto iter = Bigs.begin();
+    
+    string pick;
+    while(iter != Bigs.end()) {
+        for (int i = 0; i < 5; i++) {
+            pick = iter->second[i];
+            if (Littles.find(pick) == Littles.end()) {
+                continue;
+            }
+            else {
+                bool is_pick = false;
+                for (int j = 0; j < 5; j++) {
+                    if (Littles[pick][j] == iter->first) {
+                        vector<string> pairing;
+                        pairing.resize(2);
+                        
+                        pairing[0] = iter->first;
+                        pairing[1] = pick;
+                        
+                        duos.push_back(pairing);
+                        
+                        auto temp = iter->first;
+                        iter++;
+                        
+                        Bigs.erase(temp);
+                        Littles.erase(pick);
+                        is_pick = true;
+                        break;
+                    }
+                }
+            }
+        }
+        iter++;
+    }
+    duos.size();
+}
+
+void Pairs::random_pairs() {
+    while (!Bigs.empty() && !Littles.empty()) {
+        vector<string> pairing;
+        pairing.resize(2);
+        
+        pairing[0] = Bigs.begin()->first;
+        pairing[1] = Littles.begin()->first;
+        
+        duos.push_back(pairing);
+        
+        Bigs.erase(Bigs.begin()->first);
+        Littles.erase(Littles.begin()->first);
+    }
+    //there are more bigs than littles, add to a family
+    int count = 1;
+    while (!Bigs.empty()) {
+        duos[duos.size() - count].push_back(Bigs.begin()->first);
+        Bigs.erase(Bigs.begin()->first);
+        count++;
+    }
+    //if there are more littles than bigs, add to a family
+    while (!Littles.empty()) {
+        duos[duos.size() - count].push_back(Littles.begin()->first);
+        Littles.erase(Littles.begin()->first);
+        count++;
+    }
+}
+
+void Pairs::write_out() {
+    ofstream fout;
+    
+    //User write in file name
+    //ADD ERROR READING
+//    cout << "Enter your file name for the littles: " << '\n';
+//    cin >> file;
+    
+    //open the file
+    fout.open("BigLittlePairs!");
+    for (int i = 0; i < duos.size(); i++) {
+        for (int j = 0; j < duos[i].size(); j++) {
+            fout << duos[i][j] << " ";
+        }
+        fout << '\n';
+    }
+}
--- Pairs.hpp
+++ Pairs.hpp
@@ -31,9 +31,16 @@ public:
     void read_in();
     void first_pairs();
     void second_pairs();
-    void third_pairs(); 
+    void third_pairs();
+    void fourth_pairs();
+    void fifth_pairs();
+    void sixth_pairs();
+    void seventh_pairs();
+    void eighth_pairs();
+    void random_pairs();
+    void write_out();
 private:
     unordered_map<string, vector<string>> Bigs;
     unordered_map<string, vector<string>> Littles;
-    vector<pair<string, string>> duos;
+    vector<vector<string>> duos;
 };
